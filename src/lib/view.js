a3054a50351f8c5dc5ee69b9524e801d63d897da
James Carmichael <james@siteclick.co.uk>
2018-05-30 09:32:15 +0100

    Remove JWT auth method (only using basic) and links to non-existent CSS

--- src/lib/view.js
+++ src/lib/view.js
@@ -1,10 +1,10 @@
 console.log(__dirname)
 const Session = require('./session')
 
-function viewContextDefaults (request) {
+function viewContextDefaults(request) {
   var viewContext = {}
 
-//  console.log (getSessionAge(request));
+  //  console.log (getSessionAge(request));
 
   viewContext.session = Session.get(request)
 
@@ -14,17 +14,17 @@ function viewContextDefaults (request) {
   console.log('VIEW CONTEXT SESSION')
   console.log(viewContext.session)
 
-//  request.session.id = request.session.id || Helpers.createGUID()
-//  request.session.pageviews = request.session.pageviews + 1 || 1
+  //  request.session.id = request.session.id || Helpers.createGUID()
+  //  request.session.pageviews = request.session.pageviews + 1 || 1
 
-//  console.log(request.session)
+  //  console.log(request.session)
 
-//  viewContext.session = request.session
+  //  viewContext.session = request.session
   viewContext.pageTitle = 'Water Abstraction'
   viewContext.insideHeader = ''
   viewContext.headerClass = 'with-proposition'
   viewContext.topOfPage = null
-  viewContext.head = '<link href="public/stylesheets/overrides.css" media="screen" rel="stylesheet" /><meta name="robots" content="noindex, nofollow">'
+  viewContext.head = '<meta name="robots" content="noindex, nofollow">'
   viewContext.bodyStart = null
   viewContext.afterHeader = null
   viewContext.path = request.path
@@ -32,7 +32,7 @@ function viewContextDefaults (request) {
   viewContext.debug.connection = request.connection.info
   viewContext.debug.request = request.info
   viewContext.debug.request.path = request.path
-  viewContext.debug.session=request.yar.get('sessionTimestamp')
+  viewContext.debug.session = request.yar.get('sessionTimestamp')
   return viewContext
 }
 
