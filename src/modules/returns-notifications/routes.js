a2b597fc2874c3213e24b295996455af0448b3e0
James Carmichael <james@siteclick.co.uk>
2018-11-23 17:21:33 +0000

    Adds forms to preview and send return reminder letters using water se… (#114)
    * Adds forms to preview and send return reminder letters using water service PDF return notifications.  The preview displays the number of recipients and the licence numbers/returns matched.
    
    * Adds forms to preview and send return reminder letters using water service PDF return notifications.  The preview displays the number of recipients and the licence numbers/returns matched.

--- src/modules/returns-notifications/routes.js
+++ src/modules/returns-notifications/routes.js
@@ -17,5 +17,23 @@ module.exports = {
     method: 'POST',
     path: '/admin/returns-notifications/invitation/send',
     handler: controller.postReturnInvitationSend
+  },
+
+  getReturnReminders: {
+    method: 'GET',
+    path: '/admin/returns-notifications/reminders',
+    handler: controller.getReturnReminders
+  },
+
+  postReturnReminders: {
+    method: 'POST',
+    path: '/admin/returns-notifications/reminders',
+    handler: controller.postReturnReminders
+  },
+
+  postSendReturnReminders: {
+    method: 'POST',
+    path: '/admin/returns-notifications/reminders/send',
+    handler: controller.postSendReturnReminders
   }
 };
