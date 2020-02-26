1288d93da92fa860f1f068e93921ef732e99f2e3
Mark Swaffer <mdswaffer@gmail.com>
2020-02-26 09:32:23 +0000

    Chore: Removes the picklists forms

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,12 +1,10 @@
 const returnsRoutes = require('./returns/routes');
 const returnsNotificationRoutes = require('./returns-notifications/routes');
-const picklistRoutes = require('./picklists/routes');
 const returnsRepairRoutes = require('./returns-repair/routes');
 
 const routes = [
   ...Object.values(returnsRoutes),
   ...Object.values(returnsNotificationRoutes),
-  ...Object.values(picklistRoutes),
   ...Object.values(returnsRepairRoutes)
 ];
 
